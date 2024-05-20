5080b441a2b297a07a8086f0c7b728280f193ba1
arpit <arpitpatel009@gmail.com>
2024-05-20 15:29:01 -0400

    Initial Commit

---
+++ SwiftUiBasics/SwiftUiBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBasicsApp.swift
+//  SwiftUiBasics
+//
+//  Created by Arpit & Jinal on 2024-05-20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
